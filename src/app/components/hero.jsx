896b3b16c3b7a99cb0c15b648ecce580a3f07b89
jashDevkar <jash.devkar.1054@gmail.com>
2025-08-31 13:28:28 +0530

    nothing changed ig

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -35,63 +35,69 @@ function PrevArrow(props) {
 
 export default function Hero() {
   var settings = {
-  dots: true,
-  infinite: true,
-  speed: 600,
-  slidesToShow: 2, // default desktop
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2500,
-  nextArrow: <NextArrow />,
-  prevArrow: <PrevArrow />,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: { slidesToShow: 2 },
-    },
-    {
-      breakpoint: 400,
-      settings: { slidesToShow: 1 }, // should force 1
-    },
-  ],
-};
+    dots: true,
+    infinite: true,
+    speed: 600,
+   slidesToShow:2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
   return (
-    <section className="flex flex-col md:flex-row md:items-center justify-between px-12 w-full
+    <section className="flex flex-col md:flex-row md:items-center justify-between md:px-12 w-full px-5
       bg-gradient-to-br from-pink-100 via-pink-300 to-pink-100 pt-28 pb-16">
-      {/* Left content */}
-      <div className="md:w-1/2 space-y-6">
-        <h2 className="text-4xl md:text-5xl font-bold text-pink-700 leading-tight">
-          Discover Timeless Jewelry
-        </h2>
+      <div className="md:w-1/2 space-y-4 mb-4 md:mb-0">
+        <h1 className="text-4xl md:text-5xl font-bold text-pink-700 ">Discover new style</h1>
         <p className="text-lg text-gray-700">
           Elegant earrings, bracelets, Decor collections,and festive rakhis crafted with love by hands for your hands.
         </p>
         <Button className="bg-pink-600 hover:bg-pink-700 text-white shadow-lg rounded-full px-6 py-3">
-          Shop Now
+          Explore now
         </Button>
       </div>
 
-      {/* Carousel */}
-      <div className="md:w-1/2 w-full mt-10 md:mt-0 ">
-        <Slider {...settings}>
-          {Array.from({ length: 4 }).map((_, index) => (
-            
-              <div className="p-3" key={index}>
-                <div className="overflow-hidden rounded-lg   transition duration-300 transform hover:scale-105 bg-transparent  ">
-                <Image
-                  src={`/${index + 1}.png`}
-                  alt={`Jewelry ${index + 1}`}
-                  width={600}
-                  height={400}
-                  className="w-full h-[350px] object-cover rounded-2xl"
-                />
-              </div>
-              </div>
-            
-          ))}
-        </Slider>
-      </div>
+
+      <Slider {...settings} className="md:w-1/2 w-full">
+
+        {
+          Array.from({length:4}).map((_,index)=>(
+            <div className="p-2 rounded-2xl hover:scale-105 transition-all duration-300 overflow-hidden">
+              <img src={`/${index+1}.png`} className="h-[300px] w-full  object-cover rounded-2xl" />
+            </div>
+          ))
+        }
+
+
+      </Slider>
     </section>
   );
 }
